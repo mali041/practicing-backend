bd2f7c76e9665a04f8aad9e5434d69ec43847327
mali041 <ali041gohar@gmail.com>
2024-04-07 10:16:52 +0500

    Adding some middlewares & Implementing Custom API response and ERROR handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./config.env",
 });
-connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGO db connection faild ! ${err}`);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (err) {
+//         res.status(err.code || 500).json({ success: false , message: err.message });
+//     }
+// }
