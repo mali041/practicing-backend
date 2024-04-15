1a37a30b58a2b12d4b8237d96743b12588643e2d
mali041 <ali041gohar@gmail.com>
2024-04-15 09:20:48 +0500

    imp-cloudinary-and-multer-also-project-setup-completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+// We get file from file system. And upload it to cloudinary by it's local path
+import {v2 as cloudinary} from 'cloudinary';
+import fs from fs;
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Upload file to cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const result = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+        console.log(`File upload Son Cloudinary ${result.url}`)
+        return result;
+    } catch(error) {
+        fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
